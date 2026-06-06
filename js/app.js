af09e5f9dec71050de524fcbbc9828e960e084dc
jrt10 <88353411+jrt10@users.noreply.github.com>
2026-06-06 22:45:50 +0000

    Olho para visualização de senha

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 console.log("Nexus iniciado");
+
+const loginForm = document.getElementById("loginForm");
+const senhaInput = document.getElementById("senha");
+const toggleSenha = document.getElementById("toggleSenha");
+
+if(toggleSenha){
+    toggleSenha.addEventListener("click", () => {
+
+        if(senhaInput.type === "password"){
+            senhaInput.type = "text";
+        } else {
+            senhaInput.type = "password";
+        }
+
+    });
+}
+
+if(loginForm){
+
+    loginForm.addEventListener("submit", function(event){
+
+        event.preventDefault();
+
+        alert("Login do Nexus ainda será conectado ao Supabase.");
+
+    });
+
+}
