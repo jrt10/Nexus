793364e05da209cfcbcb21c9acd61cd882937e66
jrt10 <88353411+jrt10@users.noreply.github.com>
2026-06-30 14:41:56 -0300

    código novo

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,1 +1,21 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+const CACHE_NAME = "nexus-pwa-menu-4";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  return;
+});
